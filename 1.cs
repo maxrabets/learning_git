b984311e4f3f8f9018677e87b66a01985c8fd0d2
maxrabets <rabetsmaxim@gmail.com>
2020-10-06 21:47:34 +0300

    Added Main function

---
+++ 1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Test
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+
+}
